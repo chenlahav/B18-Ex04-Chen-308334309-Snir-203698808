06520b8e69d625008a960c59597925f3e4943d48
chen lahav <chenlahav6@gmail.com>
2018-05-11 20:04:11 +0300

    started to implements Menu.Intreface project

---
+++ Ex04.Menus.Interfaces/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    class MainMenu
+    {
+        List<MenuItem> m_MenuItems;
+
+        public MainMenu(List<MenuItem> i_MenuItems)
+        {
+            m_MenuItems = i_MenuItems;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Welcome!");
+            int selection = 0;
+            do
+            {
+                //TODO: show the options
+                Console.Write("Choose one of the options: ");
+                bool result = int.TryParse(Console.ReadLine(), out selection);
+                while (!result)
+                {
+                    Console.Write("Invalid input\nChoose one of the options: ");
+                    result = int.TryParse(Console.ReadLine(), out selection);
+                }
+                //TODO: select the user's choice
+            }
+            while (selection != 0);
+
+            Console.WriteLine("Thank you!\nBye Bye..");
+        }
+    }
+}
---
+++ Ex04.Menus.Interfaces/MenuItem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    class MenuItem
+    {
+        private readonly List<MenuItem> m_MenuItems;
+
+        private readonly List<IRunable> m_RunableObservers = new List<IRunable>();
+
+        public MenuItem(List<MenuItem> i_MenuItems)
+        {
+            m_MenuItems = i_MenuItems;
+        }
+
+        public void AttachObserver(IRunable i_RunableObserver)
+        {
+            m_RunableObservers.Add(i_RunableObserver);
+        }
+
+        public void DetachObserver(IRunable i_RunableObserver)
+        {
+            m_RunableObservers.Remove(i_RunableObserver);
+        }
+
+        public void doWhenItemWasSelected()
+        {
+            if (m_MenuItems == null)
+            {
+                showMenu();
+            }
+
+            else
+            {
+                notifyRunableObservers();
+            }
+        }
+
+        private void showMenu()
+        {
+            //TODO: show the sub menu
+        }
+
+        private void notifyRunableObservers()
+        {
+            foreach (IRunable observer in m_RunableObservers)
+            {
+                observer.Run();
+            }
+        }
+
+
+    }
+}
